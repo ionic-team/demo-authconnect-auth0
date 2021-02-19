1873781d14622a26bf7b2e84f19d2fd3265988cb
Matt Netkow <matt.netkow@gmail.com>
2021-02-18 22:09:45 -0600

    Add cap 3

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.demo.ac.auth0',
+  appName: 'demo-authconnect-auth0',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
